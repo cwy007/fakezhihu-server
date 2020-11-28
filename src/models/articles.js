559c282ca5c76af195c5b8ee992fd7b3bdcde0de
chenweiyan <chanweiyan007@gmail.com>
2020-11-28 16:56:41 +0800

    add Article and Status models

---
+++ src/models/articles.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+  const Article = sequelize.define('articles', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.CHAR
+    },
+    content: {
+      type: DataTypes.TEXT
+    },
+    excerpt: {
+      type: DataTypes.CHAR
+    },
+    creatorId: {
+      type: DataTypes.INTEGER
+    },
+    type: {
+      type: DataTypes.INTEGER
+    },
+    cover: {
+      type: DataTypes.CHAR
+    },
+    createdAt: {
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      type: DataTypes.DATE
+    }
+  });
+
+  Article.associate = (models) => {
+    Article.belongsTo(models.users, { foreignKey: 'creatorId', as: 'author' });
+    Article.hasOne(models.status, { foreignKey: 'targetId', as: 'status' })
+  }
+  return Article;
+}
---
+++ src/models/status.js
@@ -0,0 +1,31 @@
+module.exports = (sequelize, DataTypes) => {
+  const Status = sequelize.define('status', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    voteUp: {
+      type: DataTypes.TEXT
+    },
+    voteDown: {
+      type: DataTypes.TEXT
+    },
+    favorite: {
+      type: DataTypes.TEXT
+    },
+    targetId: {
+      type: DataTypes.INTEGER
+    },
+    targetType: {
+      type: DataTypes.INTEGER
+    },
+    createdAt: {
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      type: DataTypes.DATE
+    }
+  });
+  return Status;
+}
